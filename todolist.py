7921b0d987980c9df68c54fd62aca6f163cd09e7
Kaustubh Karkare <kaustubh.karkare@gmail.com>
2013-09-14 00:48:18 +0530

    Implemented periodic tasks.
    Starting from the last-run date to today, for each date, all periodic tasks are evaluated.
    If allowed, duplicate of the periodic tasks (without the periodic tags) are added to that day's tasks.
    
    The TaskList.parse/serialize no longer pretend to be static methods.
    Refactored the Task class to use only a single 'raw' data variable.

--- todolist.py
+++ todolist.py
@@ -4,11 +4,22 @@ import argparse, copy, datetime, itertools, prettytable, re, readline, os, sys
 
 class Tags:
 
-	longterm = "longterm".split()
-	periodic = "everyday weekday weekend monday tuesday wednesday thrusday friday saturday sunday".split()
+	periodic = {
+		"everyday": lambda x: True,
+		"weekday": lambda x: x.weekday()<5,
+		"weekend": lambda x: x.weekday()>4,
+		"monday": lambda x: x.weekday()==0,
+		"tuesday": lambda x: x.weekday()==1,
+		"wednesday": lambda x: x.weekday()==2,
+		"thursday": lambda x: x.weekday()==3,
+		"friday": lambda x: x.weekday()==4,
+		"saturday": lambda x: x.weekday()==5,
+		"sunday": lambda x: x.weekday()==6,
+	}
+
 	status = "done partial failed".split()
-	random = "hidden"
-	all = list(itertools.chain(longterm, periodic, status, random))
+	random = "hidden essential date".split()
+	all = list(itertools.chain(periodic, status, random))
 
 class Date:
 
@@ -21,24 +32,18 @@ class Date:
 	tomorrow = (_today+_1day).strftime(_dateformat)
 
 	pattern = "[0-9]{4}-[0-9]{2}-[0-9]{2}"
+	recognized = "today yesterday tomorrow".split()
 
 	@staticmethod
 	def translate(data):
 		if data=="today": return Date.today
 		elif data=="yesterday": return Date.yesterday
 		elif data=="tomorrow": return Date.tomorrow
-	@staticmethod
-	def recognized():
-		return "today yesterday tomorrow".split()
 
 class TaskList:
-	
+
 	# Special Groups
-	special = { "Longterm": Tags.longterm, "Periodic": Tags.periodic }
-	# Group Heading Pattern
-	re_date = re.compile("^"+Date.pattern+"|"+"|".join(special.keys())+"$")
-	# Last Run Data
-	re_last = re.compile("^# ("+Date.pattern+")$")
+	special = { "Essential":["essential"], "Periodic": Tags.periodic }
 
 	def __init__(self,datafile):
 		self.name = datafile
@@ -48,6 +53,7 @@ class TaskList:
 		if group not in self.data:
 			self.data[group] = TaskGroup(self,group)
 		return self.data[group]
+
 	def delete(self,group):
 		if group.name in self.data:
 			del self.data[group.name]
@@ -64,45 +70,68 @@ class TaskList:
 	def fileload(self):
 		if os.path.isfile(self.name):
 			f = open(self.name,"r")
-			self.data = self.parse( f.read() )
+			self.raw = f.read()
 			f.close()
 		else:
 			# raise Exception("Data File does not exist!");
-			self.data = {}
+			# TODO: Provide Warning at this point!
+			self.raw = ""
+		self.parse()
 
 	def filesave(self):
+		self.serialize()
 		i = 0
 		while True: # do
 			name = self.name+"."+str(i)
 			if not os.path.exists(name): break # while
 			else: i = i+1
 		f = open(name,"w");
-		f.write(self.serialize(self.data))
+		f.write(self.raw)
 		f.close()
 		if os.path.isfile(self.name):
 			os.unlink(self.name)
 		os.rename(name,self.name)
 	
-	def parse(self, rawdata):
+	# Group Heading Pattern
+	re_date = re.compile("^"+Date.pattern+"|"+"|".join(special.keys())+"$")
+	# Last Run Data
+	re_last = re.compile("^# ("+Date.pattern+")$")
+
+	def parse(self):
 		self.data = {}
 		group = Date.today
-		lines = rawdata.split("\n")
+		lines = self.raw.split("\n")
+		
+		# check the last line to detect when this script was last run
 		last = self.re_last.match(lines[-1])
 		if last:
 			lines.pop()
 			self.lastrun = last.group(1)
+		else:
+			self.lastrun = Date.today
+		
+		# process the remaining lines
 		for line in lines:
 			if line.strip()=="": continue
 			date = self.re_date.match(line)
 			if date!=None: group = date.group()
 			elif line.startswith("\t"):
 				Task( self.get(group), line[1:] )
-		return self.data
 
-	def serialize(self,data):
-		keys = sorted(data.keys())[::-1]
-		rawdata = "\n".join([data[name].serialize() for name in keys if not data[name].empty()])
-		return rawdata+"\n\n# "+Date.today
+		# In case the last-run was yesterday or before, advance the date counter
+		if "Periodic" in self.data:
+			now = datetime.date(*map(int,self.lastrun.split("-")))
+			while now < Date._today:
+				now += Date._1day
+				for task in self.get("Periodic"):
+					task.iteration(now,self.get)
+
+	def serialize(self):
+		keys = sorted(self.data.keys())[::-1]
+		# temp = list(itertools.takewhile(lambda x: x in TaskList.special, keys))
+		rawdata = "\n".join( [self.data[name].serialize() for name in keys
+			if not self.data[name].empty()] )
+		self.raw = rawdata+"\n\n# "+Date.today
 
 class TaskGroup:
 
@@ -112,7 +141,7 @@ class TaskGroup:
 		self.data = []
 
 	def tag(self,tag):
-		return filter(lambda x: tag in x.tag, self.data)
+		return filter(lambda x: tag in x.tags(), self.data)
 
 	def add(self,task):
 		self.data.append(task)
@@ -140,26 +169,40 @@ class TaskGroup:
 		return temp
 
 	def tabulate(self):
-		t = 2 if self.name=="Longterm" else 1 if self.name=="Periodic" else 0
-		fields = ["ID","Task","Tags",["Status","Frequency","Status"][t]]
+		t = 1 if self.name=="Periodic" else 0
+		fields = ["ID","Task","Tags",["Status","Frequency"][t]]
 		table = prettytable.PrettyTable(fields, padding_width=1)
 		for i in fields: table.align[i] = "l"
-		for index, task in enumerate(filter(lambda x: "#hidden" not in x.tags, self.data)):
-			if t==1: status = ", ".join( filter(lambda x: x in Tags.periodic, map(lambda x: x[1:], task.tags)) )
-			else: status = next((x for x in Tags.status if "#"+x in task.tags),"pending") # first match
-			tags = ", ".join( filter(lambda x: x not in Tags.all, map(lambda x: x[1:], task.tags)) )
-			table.add_row([index+1 ,task.text, tags, status ])
+		index = 0
+		for task in self:
+			tags = task.tags()
+			if "#hidden" in tags: continue
+			else: index = index+1
+			if t==1:
+				temp = map(lambda x: x[1:], tags)
+				temp = filter(lambda x: x in Tags.periodic, temp)
+				data = ", ".join(temp)
+			else:
+				temp = (x for x in Tags.status if "#"+x in tags)
+				data = next(temp, "pending") # first match
+			temp = map(lambda x: x[1:], tags)
+			temp = filter(lambda x: x not in Tags.all, temp)
+			tags2 = ", ".join( temp )
+			table.add_row([index ,task.text(), tags2, data ])
 		return "\n"+table.get_string()+"\n"
 
 	def __iter__(self):
 		self.index = 0
+		return self
 
 	def next(self):
-		if self.index <= len(self.data):
+		if self.index < len(self.data):
 			temp = self.data[self.index]
 			self.index = self.index + 1
 			return temp
-		else: del self.index
+		else:
+			del self.index
+			raise StopIteration
 	
 	def empty(self):
 		return len(self.data)==0
@@ -169,39 +212,65 @@ class TaskGroup:
 
 class Task:
 
-	re_ws = re.compile("\s+")
-	re_tag = re.compile("\\#[^ ]+")
-	re_date = re.compile("\\#date\\=(?:"+Date.pattern+"|"+"|".join(Date.recognized())+")")
+	re_date = re.compile(Date.pattern+"|"+"|".join(Date.recognized))
+	@classmethod
+	def fn_tag(self,x): return x.startswith("#")
+	@classmethod
+	def fn_date(self,x): return self.fn_tag(x) and self.re_date.match(x[1:])
+	@classmethod
+	def fn_status(self,x): return self.fn_tag(x) and x[1:] in Tags.status
+	@classmethod
+	def fn_periodic(self,x): return self.fn_tag(x) and x[1:] in Tags.periodic
 
 	def __init__(self, group, raw):
 		self.group = group
 		self.group.add(self)
 		self.update(raw)
 
+	def reject(self,fn):
+		self.raw = " ".join(filter( lambda x: not fn(x), self.raw.split() ))
+
+	def tags(self):
+		return filter(self.fn_tag, self.raw.split())
+
+	def text(self):
+		return " ".join(filter(lambda x: not self.fn_tag(x), self.raw.split()))
+
+	def tag_add(self,tag):
+		if self.fn_tag(tag):
+			self.raw = " ".join(self.raw.split()+[tag])
+
+	def tag_del(self,tag):
+		if self.fn_tag(tag):
+			self.reject(lambda x: x==tag)
+
 	def update(self,raw):
 		self.raw = raw
-
-		self.tags = map(str.lower, self.re_tag.findall(self.raw))
-		self.text = self.re_tag.sub("",self.raw)
-		self.text = self.re_ws.sub(" ",self.text).strip()
-
-		tlist = self.group.list
-
+		tlist = self.group.list # the tasklist which contains all groups
 		flag = False # is this a special group
+		tags = self.tags()
 		for key in TaskList.special:
 			if flag: break
 			for val in TaskList.special[key]:
 				if flag: break
-				if "#"+val.lower() in self.tags:
+				if "#"+val.lower() in tags:
 					flag = True
 					if self.group.name!=key:
 						self.group.delete(self)
 						self.group = tlist.get(key)
 						self.group.add(self)
 
-		# if this is a general task but with date specified
+		# in case of essential tasks, discard all periodic tags
+		if self.group.name=="Essential":
+			self.reject(self.fn_periodic)
+
+		# in case of periodic tasks, discard all status tags
+		if self.group.name=="Periodic":
+			self.reject(self.fn_status)
+
+		# if this is a general task, ensure that the date is proper
 		if not flag:
-			temp = filter(lambda x: self.re_date.match(x), self.tags)
+			temp = filter(self.fn_date, self.tags())
 			group = temp[-1][6:] if len(temp)>0 else self.group.name
 			if Date.translate(group): group = Date.translate(group)
 			elif group in TaskList.special: group = Date.today
@@ -210,10 +279,22 @@ class Task:
 				self.group = tlist.get(group)
 				self.group.add(self)
 
-		# remove all date tags
-		self.raw = self.re_date.sub("",self.raw)
-		self.raw = self.re_ws.sub(" ",self.raw).strip()
-		self.tags = filter(lambda x: not self.re_date.match(x), self.tags)
+		# in case of all tasks, remove all date tags (as they have been processed above)
+		self.reject(self.fn_date)
+
+	def iteration(self, now, group_get):
+		temp = filter( self.fn_periodic, self.tags() )
+		temp = map( lambda x: Tags.periodic[x[1:]](now), temp)
+		if any(temp):
+			# duplicate this task
+			task = copy.deepcopy(self)
+			# remove all periodic tags, since this is now a normal task
+			task.reject(self.fn_periodic)
+			# change the group
+			group = now.strftime(Date._dateformat)
+			task.group = group_get(group)
+			task.group.add(task)
+		
 
 # Command-Line Option Validators (clov)
 
@@ -281,14 +362,17 @@ def main():
 	if args.action!="list": print g.tabulate()
 	if args.action=="list": pass
 	elif args.action=="add":
-		Task(g,prompt("Create Task: "))
-		if confirm("Are you sure you want to create this task?"): l.filesave()
+		t = Task(g,prompt("Create Task: "))
+		if confirm("Are you sure you want to create this task?"):
+			t.iteration(Date._today,l.get)
+			l.filesave()
 		else: g = _g
 	elif args.action=="edit":
 		t = g.get(prompt("Select Task ID for Modification: "))
 		t.update(prompt("Update Task: ",t.raw))
 		if confirm("Are you sure you want to update this task?"):
 			l.filesave()
+			t.iteration(Date._today,l.get)
 			g = t.group
 		else: g = _g
 	elif args.action=="delete":
